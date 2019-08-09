9253944aac465e5c564db84c3b333ad36ac21264
Pedro Schmitt <pedromartins.bsb@gmail.com>
2019-08-09 09:48:02 -0300

    Set rails to not generate tests automatically

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,8 @@ module DevVue
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.generators.assets = false
+    config.generators.helper = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
