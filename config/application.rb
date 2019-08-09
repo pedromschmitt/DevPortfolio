89bc9a8b4ac7b36bdd0d0a13135ed7f84c5b753e
Pedro Schmitt <pedromartins.bsb@gmail.com>
2019-08-09 10:18:17 -0300

    Set cucumber and factory bot

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module DevVue
     config.load_defaults 5.2
     config.generators.assets = false
     config.generators.helper = false
+    config.generators.factory_bot dir: 'features/factories'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
