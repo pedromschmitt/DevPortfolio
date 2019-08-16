4ba9e66f73954a58a31d86ccd8682b31bbac8f6b
Pedro Schmitt <pedromartins.bsb@gmail.com>
2019-08-16 15:47:26 -0300

    Set models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  # :confirmable, :lockable, :timeoutable  and :omniauthable
+  devise :database_authenticatable, :registerable, :trackable,
          :recoverable, :rememberable, :validatable
+  
+  has_many :portfolios
 end
